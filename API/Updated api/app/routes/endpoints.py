141fb318dc9bfa62c0278a25210bb7218f4918ac
Sölvi Sigmarsson <solvi24@ru.is>
2026-03-22 14:27:52 +0100

    first commit

---
+++ API/Updated api/app/routes/endpoints.py
@@ -0,0 +1,16 @@
+# Task C5
+
+'''
+Part 1 - Endpoint 1: Implement get_monthly_energy_flow_data()
+to return monthly energy flow per plant and measurement type.
+'''
+
+'''
+Part 1 - Endpoint 2: Implement get_monthly_company_usage_data()
+to return monthly energy usage per company per plant.
+'''
+
+'''
+Part 1 - Endpoint 3: Implement get_monthly_plant_loss_ratios_data()
+to return average monthly loss ratios per plant.
+'''
